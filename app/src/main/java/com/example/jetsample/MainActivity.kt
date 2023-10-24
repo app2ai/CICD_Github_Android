be13e7f803513389d496bce80d92db1fa84bfc64
Vishal Rana <job.vishalrana@gmail.com>
2023-10-24 14:50:48 +0530

    added designs

--- app/src/main/java/com/example/jetsample/MainActivity.kt
+++ app/src/main/java/com/example/jetsample/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetsample.scenes.BasicForm
 import com.example.jetsample.ui.theme.JetSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,25 +24,9 @@ class MainActivity : ComponentActivity() {
                     modifier = modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android", modifier)
+                    BasicForm(modifier)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetSampleTheme {
-        Greeting("Android")
     }
 }
